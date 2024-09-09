a38a2055a5ea9eb63d6f1035d18014c51c3ed39c
ravindra_gouda <ravindragouda444@gmail.com>
2024-09-09 10:58:34 +0530

    added product model

--- src/main/java/com/example/Ecommerce/Controller/ProductController.java
+++ src/main/java/com/example/Ecommerce/Controller/ProductController.java
@@ -1,10 +1,12 @@
-package com.example.Ecommerce;
+package com.example.Ecommerce.Controller;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-public class Controller {
+@RequestMapping("/api")
+public class ProductController {
     @GetMapping("/")
     public String greet(){
         return "Hello";
---
+++ src/main/java/com/example/Ecommerce/Model/Product.java
@@ -0,0 +1,26 @@
+package com.example.Ecommerce.Model;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.math.BigDecimal;
+import java.util.Date;
+@Entity
+@AllArgsConstructor
+@NoArgsConstructor
+@Data
+public class Product {
+    @Id
+    private  int id;
+    private String name;
+    private String desc;
+    private  String brand;
+    private BigDecimal price;
+    private String category;
+    private Date releaseDate;
+    private boolean availability;
+    private int quantity;
+}
